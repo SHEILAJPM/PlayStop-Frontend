1ba501fb05b506c5d64702c9c05429569f54707e
Sheila jpm <principemerinosheila@gmail.com>
2026-06-08 22:35:04 -0500

    Crear archivo de constantes globales: roles, deportes y duraciones

---
+++ src/utils/constants.js
@@ -0,0 +1,20 @@
+﻿export const APP_NAME = 'PlayStop';
+export const APP_VERSION = '1.0.0';
+
+export const ROLES = {
+  JUGADOR: 'USER',
+  PROPIETARIO: 'OWNER',
+  ADMIN: 'ADMIN',
+  SUPER_ADMIN: 'SUPER ADMIN',
+};
+
+export const DEPORTES = [
+  'Futbol',
+  'Padel',
+  'Tenis',
+  'Basketball',
+  'Volleyball',
+  'Squash',
+];
+
+export const DURACIONES = [30, 60, 90, 120];
