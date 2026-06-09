6d560634aa37104f862b8f4d0706414e5f0a9e73
Sheila jpm <principemerinosheila@gmail.com>
2026-06-08 22:35:05 -0500

    Crear utilidades de validacion para email, contrasena y telefono

---
+++ src/utils/validators.js
@@ -0,0 +1,8 @@
+﻿export const isValidEmail = (email) =>
+  /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+export const isValidPassword = (password) =>
+  password.length >= 8;
+
+export const isValidPhone = (phone) =>
+  /^\+?[0-9]{7,15}$/.test(phone);
