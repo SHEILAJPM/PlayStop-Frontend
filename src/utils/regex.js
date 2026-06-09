9c105bba24d35c297049f3524c65c9d46e33db7a
Sheila jpm <principemerinosheila@gmail.com>
2026-06-08 22:35:14 -0500

    Centralizar expresiones regulares de validacion en modulo regex.js

---
+++ src/utils/regex.js
@@ -0,0 +1,8 @@
+﻿export const REGEX = {
+  email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+  password: /^(?=.*[A-Z])(?=.*[0-9]).{8,}$/,
+  phone: /^\+?[0-9]{7,15}$/,
+  onlyLetters: /^[a-zA-ZÃ€-Ã¿\s]+$/,
+  onlyNumbers: /^[0-9]+$/,
+  url: /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/,
+};
